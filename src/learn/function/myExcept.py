10ea71d858baa2b0ab5c56b9cef1a1d670e0f3a4
zhaofeng <zhaofeng@altamob.com>
2017-01-03 16:12:05 +0800

    test 冲突

--- src/learn/function/myExcept.py
+++ src/learn/function/myExcept.py
@@ -7,7 +7,7 @@ from learn.function.exceptionTest import MyError
 
 try:
     raise MyError(2*2)
-    print("1")
+    print("b")
 except MyError as err:
     print('My exception occurred, value:', err.value)
     
